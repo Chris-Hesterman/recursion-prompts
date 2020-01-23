6f8fb9fce76a288d90a438a75f47a9b2443a52f5
Chris Hesterman <spoiler14@comcast.net>
2020-01-23 13:36:40 -0800

    Implement code to pass all tests for replaceKeysInObj, recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -344,6 +344,19 @@ var countValuesInObj = function(obj, value) {
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+    let count = 0;
+
+    if (typeof obj === 'object' && obj !== null) {
+        let keys = Object.keys(obj);
+        for (let item of keys) {
+            if (item === oldKey) {
+                obj[newKey] = obj[oldKey];
+                delete obj[oldKey];
+            }
+            replaceKeysInObj(obj[item], oldKey, newKey);
+        } 
+    } 
+    return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
